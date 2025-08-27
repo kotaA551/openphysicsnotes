907d19e58af5663dea7547a7224d7f2671c367cd
kotaamano <kotaamano@outlook.com>
2025-08-27 21:41:57 +0900

    chore: batch updates before deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-import path from 'path';
+import path from 'path'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   webpack: (config) => {
-    // teach the bundler that '@' is your project root
     config.resolve.alias['@'] = path.resolve(process.cwd());
     return config;
   },
