b14fa3f2c5d229ec1c5723a1f015393f20d1411e
kotaamano <kotaamano@outlook.com>
2025-08-27 17:28:07 +0900

    chore: batch updates before deploy

--- lib/chapters.ts
+++ lib/chapters.ts
@@ -3,7 +3,7 @@ export const chapters = [
   { slug: "2-Electromagnetics",        title: "2-Electromagnetics" },
   { slug: "3-Relativity",              title: "3-Relativity" },
   { slug: "4-Quantum-mechanics",       title: "4-Quantum mechanics" },
-  { slug: "5-Quantum-field theory",    title: "5-Quantum field theory" },
+  { slug: "5-Quantum-field-theory",    title: "5-Quantum field theory" },
   { slug: "6-Gauge-theory",            title: "6-Gauge theory" },
   { slug: "7-Quantum-gravity-theory",  title: "7-Quantum gravity theory" },
 ];
