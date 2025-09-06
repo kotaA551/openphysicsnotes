f192f82a200fa5308c64459fc614b77a41a38cfa
kotaamano <kotaamano@outlook.com>
2025-09-07 07:45:25 +0900

    deploy: ship to vercel

--- lib/curiosities.ts
+++ lib/curiosities.ts
@@ -1,9 +1,9 @@
 export const curiosities = [
-  { slug: "string-theory",  title: "String Theory" },
-  { slug: "loop-quantum-gravity",  title: "Loop Quantum Gravity" },
-  { slug: "black-hole-information-paradox",  title: "Black Hole Information Paradox" },
-  { slug: "dark-matter",  title: "Dark Matter" },
-  { slug: "dark-energy",  title: "Dark Energy" },
-  { slug: "navier-stokes-equation",  title: "Navier Stokes Equation" },
-  { slug: "unifying-maxwells-equations",  title: "Unifying the Four Maxwell Equations" },
+  { slug: "string-theory",                   title: "|String Theory" },
+  { slug: "loop-quantum-gravity",            title: "|Loop Quantum Gravity" },
+  { slug: "black-hole-information-paradox",  title: "|Black Hole Information Paradox" },
+  { slug: "dark-matter",                     title: "|Dark Matter" },
+  { slug: "dark-energy",                     title: "|Dark Energy" },
+  { slug: "navier-stokes-equation",          title: "|Navier Stokes Equation" },
+  { slug: "differential-geometry",           title: "|Differential Geometry" },
 ];
